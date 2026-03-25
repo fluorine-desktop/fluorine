c7b04399c9418d907b0d19d2f8092322ee626e92
Mark Wilson <MW0901@proton.me>
2026-03-24 22:34:35 -0700

    did some project structuring

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Vala dev environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        vala
+        meson
+        ninja
+        pkg-config
+        gtk4
+        glib
+        gobject-introspection
+      ];
+    };
+  };
+}
